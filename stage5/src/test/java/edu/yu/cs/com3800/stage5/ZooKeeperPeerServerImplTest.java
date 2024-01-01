6c9f014a06dd8e389d0c2f483324707ae7b2f4a7
Noam Mayerfeld <nmayerfe@mail.yu.edu>
2024-01-01 14:00:20 -0500

    stage 5 commit 5 - include bash script

--- stage5/src/test/java/edu/yu/cs/com3800/stage5/ZooKeeperPeerServerImplTest.java
+++ stage5/src/test/java/edu/yu/cs/com3800/stage5/ZooKeeperPeerServerImplTest.java
@@ -72,7 +72,7 @@ public class ZooKeeperPeerServerImplTest {
                     .build();
             requestResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (URISyntaxException | IOException | InterruptedException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.toString());
             e.printStackTrace();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             PrintStream printStream = new PrintStream(outputStream);
